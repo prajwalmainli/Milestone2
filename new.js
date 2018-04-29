757a615fcdb3e95d27c868153bdbac8623e0bd43
prajwalmainli <36972486+prajwalmainli@users.noreply.github.com>
2018-04-30 02:35:38 +0930

    Add files via upload

---
+++ new.js
@@ -0,0 +1,15 @@
+$(".toggle").click(function() {
+      $("ul").slideToggle();
+      $("ul ul").css("display", "none");
+});
+
+$("ul li").click(function() {
+      $("ul ul").slideUp();
+      $(this).find('ul').slideToggle();
+});
+
+$(window).resize(function() {
+      if($(window).width() > 768) {
+            $("ul").removeAttr('style');
+      }
+});
